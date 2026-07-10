a5aba583919c9c68d81777b6cb2cd81420ef04bd
arditarizk <arditarizkif@gmail.com>
2026-07-11 03:15:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// ==========================================
+// KONFIGURASI UTAMA SISTEM TERATEPAY
+// ==========================================
+
+const TERATE_CONFIG = {
+    // URL Default Google Apps Script
+    API_URL: 'https://script.google.com/macros/s/AKfycbzFfHr42XmOTdehUsRo10bBms5n5k22bso6EcjEpHjLiqxqndYkhUnTnVJqbY2Qtc4K/exec',
+    
+    // Kunci Default Gemini AI (v1)
+    GEMINI_API_KEY: 'AIzaSyAfSIqmU3-LKrp2xEKlQpc9_5yp9LytXRM',
+    
+    // Informasi Dasar Aplikasi
+    APP_NAME: 'TeratePay Rayon Japanan',
+    APP_VERSION: '2.0.0'
+};
